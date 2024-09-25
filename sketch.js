3d71bbb880f7b4c759c0e053377181d601132cea
Derick Ostrenko <dostrenko@lsu.edu>
2024-09-25 13:44:59 -0500

    cirlce red works

--- sketch.js
+++ sketch.js
@@ -1,16 +1,21 @@
 function setup() {
   createCanvas(400, 400);
   sy = height;
+  cx = width / 2;
+  cy = height / 2;
+  cd = 50;
 }
 
 let sx = -10;
 let sy;
 let fire = false;
+let cx, cy, cd;
 
 function draw() {
   background(220);
 
   if(fire) {
+    fill(0);
     square(sx, sy, 5)
     sy = sy - 10;
     print("FIRE!")
@@ -20,6 +25,16 @@ function draw() {
     fire = false;
     sy = height;
   }
+
+  fill(0, 255, 0);
+
+
+  if(dist(sx, sy, cx, cy) < cd) {
+    fill(255, 0, 0)
+    fire = false;
+  }
+
+  circle(cx, cy, cd)
 
 }
 
